0b795a5ebaec36be80c1fb2493a06981ea4a8e4e
Luís Davi Gomes de Santana <101806624+yasmimx@users.noreply.github.com>
2022-12-01 11:07:44 -0300

    AddCod - Shared

--- app/src/main/java/com/example/appharrypotter/MainActivity.java
+++ app/src/main/java/com/example/appharrypotter/MainActivity.java
@@ -1,14 +1,64 @@
 package com.example.appharrypotter;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static com.example.appharrypotter.ClassLocation.latitude;
+import static com.example.appharrypotter.ClassLocation.longitude;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton btn1;
+    ImageButton btn2;
+    ImageButton btn3;
+    ImageButton btn4;
+
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1 = findViewById(R.id.corvinalbtn);
+        btn2 = findViewById(R.id.grifibtn);
+        btn3 = findViewById(R.id.sonsebtn);
+        btn4 = findViewById(R.id.lufabtn);
+
+        sharedPreferences = getSharedPreferences("MODE", Context.MODE_PRIVATE );
+
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_NETWORK_STATE}, 1);
+        }
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        LocationListener locationListener = new ClassLocation();
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            double latitude = ClassLocation.latitude;
+            double longitude = ClassLocation.longitude;
+        }
     }
+
+    public void openCorvinal(View view){
+        Intent intent = new Intent(this, home.class);
+        startActivity(intent);
+    }
+
 }
