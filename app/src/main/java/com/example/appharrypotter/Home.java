d16c99f852b656a60a7db4ccb83d7f4ac7bcd19b
Maria Cavalcante <maria.nascimento343@etec.sp.gov.br>
2022-12-02 18:53:44 -0300

    TestePatrono1

--- app/src/main/java/com/example/appharrypotter/Home.java
+++ app/src/main/java/com/example/appharrypotter/Home.java
@@ -1,14 +1,31 @@
 package com.example.appharrypotter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Home extends AppCompatActivity {
+
+    ImageButton btnTeste;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+       
+
+        getSupportActionBar().hide();
+
+        btnTeste = findViewById(R.id.btnTeste);
+    }
+
+    public void openTeste(View view) {
+        Intent intent = new Intent(this, Teste.class);
+        startActivity(intent);
     }
 }
